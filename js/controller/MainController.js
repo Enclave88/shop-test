da1f3d7fe4f3d5d7583c5447b43d29010ecd5570
Sutangu <sutangu@gmail.com>
2018-05-17 18:49:04 +0600

    switch to guitar shop

--- js/controller/MainController.js
+++ js/controller/MainController.js
@@ -2,40 +2,40 @@ app.controller('MainController', ['$scope', function ($scope) {
 
     $scope.books = [
         {
-            name: 'jQuery Trickshots',
+            name: 'Gibson guitar',
             author: 'Martin Angelov',
             price: 4.95,
-            cover: 'img/book1.jpg'
+            cover: 'img/guitars/1.jpg'
         },
         {
             name: 'Eloquent JavaScript',
             author: 'Marijn Haverbeke',
             price: 9.95,
-            cover: 'img/book2.jpg'
+            cover: 'img/guitars/2.jpg'
         },
         {
             name: 'JavaScript Patterns',
             author: 'Stoyan Stefanov',
             price: 5.35,
-            cover: 'img/book3.jpg'
+            cover: 'img/guitars/3.jpg'
         },
         {
             name: 'Effective JavaScript',
             author: 'David Herman',
             price: 4.95,
-            cover: 'img/book4.jpg'
+            cover: 'img/guitars/4.jpg'
         },
         {
             name: 'jQuery in Action',
             author: 'Bear Bibeault, Yehuda Katz',
             price: 14.95,
-            cover: 'img/book5.jpg'
+            cover: 'img/guitars/5.jpg'
         },
         {
             name: 'The Art of Unit Testing',
             author: 'Roy Osherove',
             price: 1.95,
-            cover: 'img/book6.jpg'
+            cover: 'img/guitars/6.jpg'
         }
     ];
 }]);
